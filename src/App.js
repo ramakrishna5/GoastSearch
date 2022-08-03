d04339295b763c7d59c14a86f086cf5375dbc822
Rama Krishna <ramakrishna@admins-MacBook-Pro.local>
2022-08-03 12:00:19 -0500

    Able to load and search the people

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+import './App.css';
+import CardLess from "./components/card-list/card-list.component";
+import SearchBox from './components/search-box/search-box.component';
+
+
+const App = () => {
+
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, [])
+  
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+  
+  console.log('render')
+
+  const onSearchChange =(event) => {
+  const searchFieldString = event.target.value.toLocaleLowerCase();
+  setSearchField(searchFieldString);
+  
+  }
+
+
+  return (
+    <div className="App">
+        <h1 className='app-title'>Monster Search</h1>
+        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters'
+        className= 'monsters-search-box'/>
+        <CardLess monsters={filteredMonsters}/>
+      </div>
+  )
+}
+
+export default App;
